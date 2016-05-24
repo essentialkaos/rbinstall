676481a991365a2292547d4034f09da66468173b
Anton Novojilov <andy@essentialkaos.com>
2016-05-24 18:54:37 -0400

    UI improvements

--- cli/task.go
+++ cli/task.go
@@ -28,8 +28,8 @@ type Task struct {
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 var (
-	spinnerFrames = []string{"⢰", "⣰", "⣠", "⣀", "⣄", "⣆", "⡆", "⠇", "⠏", "⠋", "⠉", "⠙", "⠹", "⠸"}
-	framesDelay   = []time.Duration{40, 30, 20, 30, 40, 50, 50, 50, 50, 60, 70, 60, 50, 50}
+	spinnerFrames = []string{"⠸", "⠴", "⠤", "⠦", "⠇", "⠋", "⠉", "⠙"}
+	framesDelay   = []time.Duration{60, 40, 30, 40, 60, 60, 60, 60}
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -54,7 +54,7 @@ func (t *Task) showSpinner() {
 SPINNERLOOP:
 	for {
 		for i, frame := range spinnerFrames {
-			fmtc.Printf("{c}%s{!} %s", frame, t.Desc)
+			fmtc.Printf("{y}%s{!} %s", frame, t.Desc)
 			time.Sleep(framesDelay[i] * time.Millisecond)
 			fmtc.Printf("\r")
 
@@ -78,6 +78,6 @@ func (t *Task) hideSpinner(ok bool) {
 	if ok {
 		fmtc.Printf("{g}✔{!} %s {s}(%s){!}\n", t.Desc, timeutil.PrettyDuration(time.Since(t.start)))
 	} else {
-		fmtc.Printf("{r}✘{!} %s {s}(%s){!}\n\n", t.Desc, timeutil.PrettyDuration(time.Since(t.start)))
+		fmtc.Printf("{r}✖{!} %s {s}(%s){!}\n\n", t.Desc, timeutil.PrettyDuration(time.Since(t.start)))
 	}
 }
